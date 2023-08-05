ec45e2c4d8fefa761e7135d169815805ad859aaa
Sean Ghaeli <seanghaeli@dhcp-128-189-214-162.ubcsecure.wireless.ubc.ca>
2023-08-04 17:22:21 -0700

    Refactored PLE's call method for type compatibility

--- tabtransformertf/models/embeddings.py
+++ tabtransformertf/models/embeddings.py
@@ -45,8 +45,9 @@ class PLE(tf.keras.layers.Layer):
 
         for i in range(1, self.n_bins + 1):
             i = tf.constant(i)
-            left_mask = (x < self.lookup_table.lookup(i - 1)) & (i > 1)
-            right_mask = (x >= self.lookup_table.lookup(i)) & (i < self.n_bins)
+            x_float = tf.cast(x, dtype=tf.float32)
+            left_mask = (x_float < self.lookup_table.lookup(i - 1)) & (i > 1)
+            right_mask = (x_float >= self.lookup_table.lookup(i)) & (i < self.n_bins)
             v = (x - self.lookup_table.lookup(i - 1)) / (
                 self.lookup_table.lookup(i) - self.lookup_table.lookup(i - 1)
             )
